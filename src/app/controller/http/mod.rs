b2bfe55d975b3188cf78115e2e41baa446d3a93b
Chongyi <xpz3847878@163.com>
2022-03-22 01:08:24 +0800

    调整接口前缀

--- src/app/controller/http/mod.rs
+++ src/app/controller/http/mod.rs
@@ -1,10 +1,13 @@
-use axum::{Router, routing::get};
 use self::content::*;
+use axum::{routing::get, Router};
 
 pub mod content;
 
 pub fn routes() -> Router {
-    Router::new()
-        .route("/content", get(get_content_list))
-        .route("/content/:index", get(get_content))
+    Router::new().nest(
+        "/api",
+        Router::new()
+            .route("/content", get(get_content_list))
+            .route("/content/:index", get(get_content)),
+    )
 }
